7ee2e6799bb39c6cd207449ae90f7c9602b5e9ad
qwsq2008 <quinn@DESKTOP-4OB6FOK.home>
2021-06-09 20:56:12 -0500

    adds dragon axe and models for the dragon pick,hoe,andshovel

---
+++ src/main/java/net/mcreator/hatsmpcustomitemsandblocks/item/DragonaxeItem.java
@@ -0,0 +1,74 @@
+
+package net.mcreator.hatsmpcustomitemsandblocks.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import net.mcreator.hatsmpcustomitemsandblocks.itemgroup.HATSMPItemGroup;
+import net.mcreator.hatsmpcustomitemsandblocks.HatSmpModElements;
+
+@HatSmpModElements.ModElement.Tag
+public class DragonaxeItem extends HatSmpModElements.ModElement {
+	@ObjectHolder("hat_smp:dragonaxe")
+	public static final Item block = null;
+	public DragonaxeItem(HatSmpModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1000;
+			}
+
+			public float getEfficiency() {
+				return 13f;
+			}
+
+			public float getAttackDamage() {
+				return 13f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 10;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(DragonpasteItem.block, (int) (1)));
+			}
+		}, 1, 1.1f, new Item.Properties().group(HATSMPItemGroup.tab).isImmuneToFire()) {
+			@Override
+			public boolean hasContainerItem() {
+				return true;
+			}
+
+			@Override
+			public ItemStack getContainerItem(ItemStack itemstack) {
+				return new ItemStack(this);
+			}
+
+			@Override
+			public boolean isRepairable(ItemStack itemstack) {
+				return false;
+			}
+
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("dragonaxe"));
+	}
+}
